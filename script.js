d9f1976bbc65fd163d20b2300601ced119f84696
jairoalves2741 <jairomachadoalves35@gmail.com>
2025-09-25 22:28:57 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/PWA_game/sw.js")
+    navigator.serviceWorker.register("/sw.js")
         .then(() => console.log("Service Worker registrado!"))
         .catch((err) => console.error("Erro no registro do Service Worker:", err));
 }
--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener("install", () => {
     caches.open("CacheStorageAPI").then((cache) => {
         return cache.addAll([
-            "/PWA_game/index.html",
-            "/PWA_game/game-logic.js",
-            "/PWA_game/manifest.json",
-            "/PWA_game/script.js",
-            "/PWA_game/style.css",
-            "/PWA_game/sw.js",
-            "/PWA_game/icons/icon-192x192.png",
-            "/PWA_game/icons/icon-512x512.png"
+            "/index.html",
+            "/game-logic.js",
+            "/manifest.json",
+            "/script.js",
+            "/style.css",
+            "/sw.js",
+            "/icons/icon-192x192.png",
+            "/icons/icon-512x512.png"
         ]);
     })
 })
