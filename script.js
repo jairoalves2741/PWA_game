f98e1d237671fd0ec4b708c295f977e6a7a13695
Jairo M A do carmo <jairomachadoalves35@gmail.com>
2025-09-25 22:12:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js")
+    navigator.serviceWorker.register("/PWA_game/sw.js")
         .then(() => console.log("Service Worker registrado!"))
         .catch((err) => console.error("Erro no registro do Service Worker:", err));
 }
